f2daecebe697258a3466251691ffde5b020a76d7
wregen <W.Regenczuk@yahoo.com>
2013-11-20 15:59:08 +0100

    first change

--- www/js/data/data.php
+++ www/js/data/data.php
@@ -1,3 +1,13 @@
 <?php
 
-?>
+$db = new PDO('sqlite:./bahus.sqlite');
+$sth = $db->query('SELECT * FROM bahus;');
+$out = array();
+foreach ($sth as $row) {
+    $out[] = array(
+        'id' => $row['id'],
+        'name' => $row['name'],
+        'path' => './img/bahus/' . $row['name'].'.jpg',
+    );
+}
+echo json_encode($out);
