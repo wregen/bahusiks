46025d59efde5aaefe9771b73293ed399215ead0
wregen <W.Regenczuk@yahoo.com>
2013-11-21 04:46:04 +0100

    First iteratioin of data cleaning

--- www/js/data/data.php
+++ www/js/data/data.php
@@ -1,13 +1,13 @@
 <?php
 
-$db = new PDO('sqlite:./bahus.sqlite');
-$sth = $db->query('SELECT * FROM bahus;');
+//
+$db = new PDO('sqlite:./data.sqlite');
+$db->exec('CREATE  TABLE "bahus" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "name" TEXT, "path" TEXT, "author" TEXT, "street" TEXT, "desc" TEXT, "latlon" TEXT);');
+$sth = $db->query('SELECT * FROM bahus;', PDO::FETCH_ASSOC);
 $out = array();
 foreach ($sth as $row) {
-    $out[] = array(
-        'id' => $row['id'],
-        'name' => $row['name'],
-        'path' => './img/bahus/' . $row['name'].'.jpg',
-    );
+    $out[] = $row;
 }
-echo json_encode($out);
+
+file_put_contents("data.json", json_encode($out));
+
